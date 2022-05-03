311dd306f03c4b472dc8867a6281848006f52823
harryqas <harryqas94@gmail.com>
2022-05-03 22:44:47 +0900

    add regist page, list page

--- src/main/java/com/spring/web/controller/BoardController.java
+++ src/main/java/com/spring/web/controller/BoardController.java
@@ -4,30 +4,54 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.spring.web.model.BoardVO;
+import com.spring.web.service.BoardService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
-@RequestMapping("/board/")
+@RequestMapping("/board/*")
 public class BoardController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	@Autowired
+	private BoardService boardService;
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@GetMapping(value = "/list")
-	public String home(Locale locale, Model model) {
-		logger.info("여기는 board/list");				
-		
-		model.addAttribute("serverTime", "뭘보노" );
-		
-		return "board/board";
-	}
+	private static final Logger log = LoggerFactory.getLogger(BoardController.class);
 	
+	 @GetMapping("/list")
+    // => @RequestMapping(value="list", method=RequestMethod.GET)
+    public void boardListGET() {
+        
+        log.info("게시판 목록 페이지 진입");
+        
+    }
+    
+    @GetMapping("/regist")
+    // => @RequestMapping(value="enroll", method=RequestMethod.GET)
+    public void boardReigstGET() {
+        
+        log.info("게시판 등록 페이지 진입");
+        
+    }	 
+
+    /* 게시판 등록 */
+    @PostMapping("/regist")
+    public String boardReigstPOST(BoardVO board, RedirectAttributes rttr) {
+        
+        log.info("BoardVO : " + board);
+        boardService.registBoard(board);
+        
+        rttr.addFlashAttribute("result", "regist success");
+        
+        return "redirect:/board/list";
+    }
 }
